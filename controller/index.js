6bcd5ba5fbe3ae867687e8c83aed78d4d2c6fc5c
xiali <19972854940@163.com>
2018-11-09 20:27:41 +0800

    完成了首页分类页面

--- controller/index.js
+++ controller/index.js
@@ -1,9 +1,26 @@
+const connection = require('../db/index')
 module.exports = {
     handleIndex(req, res) {
         // console.log(req.session.user);
-        res.render('index.html', {
-            user:req.session.user,
-            isLogin:req.session.isLogin
+        let pageSize = 3;
+        let currentPage = parseInt(req.query.page) || 1
+        /** @namespace req.query.page */
+        const sql = `select a.id, a.title, a.ctime, u.nickname, u.username from articles as a
+    left join users as u
+    on a.author_id = u.id
+    order by a.id desc
+    limit ${(currentPage - 1) * pageSize}, ${pageSize};
+    select count(*) as count from articles;`
+        connection.query(sql, (err, result) => {
+            const totalPage = Math.ceil(parseInt(result[1][0].count) / pageSize);
+            res.render('index.html', {
+                user: req.session.user,
+                isLogin: req.session.isLogin,
+                article: result[0],
+                totalPage: totalPage,
+                currentPage: currentPage
+            })
         })
+
     }
 };
